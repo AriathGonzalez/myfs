2deeba1307db7137e0e4306b38554f6dc4fefb52
AriathGonzalez <sab.thefourth@gmail.com>
2024-03-25 00:51:01 -0400

    Actually 1) done

--- implementation.h
+++ implementation.h
@@ -3,123 +3,64 @@
 
 #include <stdint.h>
 
-/*
-+--------------------------------------------------+
-|               Disk Partition Layout              |
-+--------------------------------------------------+
-|   Superblock (1 block)                           |
-|   Size: 1024 Bytes                               |
-|   Address: 0x0000                                |
-+--------------------------------------------------+
-|   Inode Bitmap (2 blocks)                        |
-|   Size: 2,048 Bytes                              |
-|   Address: 0x0400                                |
-+--------------------------------------------------+
-|   Block Bitmap (13 blocks)                       |
-|   Size: 13,312 Bytes                             |
-|   Address: 0x0C00                                |
-+--------------------------------------------------+
-|   Inode Table (2500 blocks)                      |
-|   Size: 2,560,000 Bytes                          |
-|   Address: 0x4000                                |
-+--------------------------------------------------+
-|   Data Region (Remaining)                        |
-|   Address: 0x2800000                             |
-+--------------------------------------------------+
-*/
+// Constants and type definitions
+#define MAGIC_NUMBER ((uint32_t)0xADDBEEF)
+#define NAME_MAX_LEN ((size_t)255)
+#define BLOCK_SIZE ((size_t)1024)
 
-/* Definitions and type declarations */
+typedef uint64_t offset;  
+typedef unsigned int u_int;
 
-// Magic number
-#define MAGIC_NUMBER ((uint32_t)0xaddbeef)
+/* Struct Declarations (1) */
 
-// Maximum length of a file name
-#define MAX_FILE_NAME_LENGTH 255
+// Superblock structure
+typedef struct superblock {
+    uint32_t magic_number; // Magic number identifying the file system
+    offset free_memory;    // Offset to the free memory
+    offset root_directory; // Offset to the root directory
+    size_t size;           // Total size of the file system
+} superblock_t;
 
-// Maximum number of inodes
-#define MAX_FILE_NUM 10000  
-
-// Maximum number of simultaneously open files
-#define MAX_OPEN_FILES 10
-
-// Start block for inode bitmap
-#define INODE_MAP_START 1  
-
-// Start block for block bitmap
-#define BLOCK_MAP_START 3 
-
-// Start block for inode table
-#define INODE_TABLE_START 16 
-
-// Start block for block table
-#define BLOCK_TABLE_START 2516 
-
-// Block size in bytes
-#define BLOCK_SIZE 1024
-
-// File and directory types
-#define FILE_TYPE 1
-#define DIRECTORY_TYPE 2
-
-// Maximum directory levels
-#define MAX_LEVEL 10  
-
-// File descriptor array for open files
-int fd[MAX_OPEN_FILES]; 
-
-// Current working directory name
-char cwd_name[100]; 
-
-// Current working directory's inode number
-int cwd_inode;  
-
-/* Memory Allocation STRUCTS */
-
-// Super block structure
-typedef struct super_block {
-  uint32_t magic_number; // Magic number to identify filesystem type
-  uint32_t block_num;    // Total number of data blocks
-  uint32_t inode_num;    // Total number of inode blocks
-  uint32_t free_block;   // Total number of free data blocks
-  uint32_t free_inode;   // Total number of free inode blocks
-} super_block;
-
-// Bitmap structure for block allocation
-typedef struct bmap {
-  unsigned char map[BLOCK_SIZE]; // Bitmap array
-} bmap;
-
-// Inode structure
+// Inode structure (common fields for both files and directories)
 typedef struct inode {
+    char name[NAME_MAX_LEN + 1];  // Name of the file or directory
+    struct timespec accessed_time; // Last access time
+    struct timespec modified_time; // Last modification time
     uint8_t type;                // Type: 1 for file, 2 for directory
-    uint32_t num;                // Inode number
-    uint64_t size;               // Size of file in bytes
-    uint32_t uid;                // User ID
-    uint32_t gid;                // Group ID
-    char mode[11];               // File permissions
-    char name[MAX_FILE_NAME_LENGTH];  // File name
-    uint32_t blocks[10];         // Direct blocks (10 KB each)
-    uint32_t ind_blocks[30];     // Indirect blocks (7.5 MB)
-    uint8_t unused[15];          // Padding to align struct size
-} inode;
+    union {
+        inode_file_t file;         // File-specific inode fields
+        inode_directory_t directory; // Directory-specific inode fields
+    } value;
+} inode_t;
 
-// Structure for indirect blocks
-typedef struct ind_block {
-  int blocks[256]; // Pointers to data blocks
-} ind_block;
+// File-specific inode fields
+typedef struct inode_file {
+    size_t size;      // Size of the file
+    offset first_block; // Offset to the first block of the file
+} inode_file_t;
 
-// Directory entry structure
-typedef struct directory_entry {
-  int inode;       // Inode number
-  int type;        // Type: file or directory
-  int length;      // Length of file name
-  char name[20];   // File or directory name
-} directory_entry;
+// Directory-specific inode fields
+typedef struct inode_directory {
+    size_t num_children; // Number of children in the directory
+    offset children;     // Offset to the children list
+} inode_directory_t;
 
-// Directory block structure
-typedef struct directory {
-  struct directory_entry directory_entries[32]; // Directory entries
-} directory;
+// Memory block structure
+typedef struct data_block {
+    size_t total_size; // Total size of the memory block
+    size_t user_size;  // Size of the memory block for user data
+    offset next;       // Offset to the next memory block
+} data_block_t;
+
+// File block structure
+typedef struct file_block {
+    size_t size;      // Size of the file block
+    size_t allocated; // Allocated size of the file block
+    offset next;      // Offset to the next file block
+    offset data;      // Offset to the data of the file block
+} file_block_t;
+
+/* END Struct declarations (1) */
 
 /* Start fuse helper methods */
 
@@ -157,7 +98,7 @@ int __myfs_utimens_implem(void *fsptr, size_t fssize, int *errnoptr,
                           const char *path, const struct timespec ts[2]);
 int __myfs_statfs_implem(void *fsptr, size_t fssize, int *errnoptr,
                          struct statvfs *stbuf);
-                         
+
 // END of fuse function declarations
 
 #endif
